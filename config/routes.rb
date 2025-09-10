e6cb0eedec885e65322310b5c222c94d7ec4d115
Dio Arayan <drarayan1994@gmail.com>
2025-09-10 13:35:53 +0800

    add status enum in blogs table

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   resources :portfolio
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :blogs do
+    member do
+      get :toggle_status
+    end 
+  end
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
