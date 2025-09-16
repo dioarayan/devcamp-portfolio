4cf9b666c96142309c65eabd7ef41e1da7fad0e3
Dio Arayan <drarayan1994@gmail.com>
2025-09-17 03:05:56 +0800

    Confiugred custom route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   resources :portfolios
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
-  root "blogs#index"
+
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
 
   resources :blogs
+
+  root "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
